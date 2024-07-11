841d23ff67c95dbec9b5b9195ba9f00371020c79
Dhruv-krishn-a <dhruvkrishna1055@gmail.com>
2024-07-11 23:58:43 +0530

    Removing Tailwind errors.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black">
-      <h1>Hello PortFolio</h1>
+    <main className="bg-white">
+      
     </main>
   );
 }
